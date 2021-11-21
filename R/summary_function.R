e6047322da18f7d24976ca465f26094154cc1816
Charlotte Barclay <cbarcl01@student.ubc.ca>
2021-11-20 16:31:56 -0800

    update code to call dplyr in function

--- R/summary_function.R
+++ R/summary_function.R
@@ -4,9 +4,9 @@
 #' @description This function will provide summary statistics, for a numerical variable y when grouped by another variable x.
 #' The statistics generated are; minimum, maximum, average (mean), range and standard deviation.
 #'
-#' @params data - refers to the dataset you want to create the summary statistics for.
-#' @params cat - refers to the category that you wish to group by.
-#' @params y - refers to the numerical variable that you wish to summarise, in relation to the groups defined in cat.
+#' @param data - refers to the dataset you want to create the summary statistics for.
+#' @param cat - refers to the category that you wish to group by.
+#' @param y - refers to the numerical variable that you wish to summarise, in relation to the groups defined in cat.
 #'
 #' @return A summary table providing the  minimum, maximum, average (mean), range and standard deviation of variable y as grouped by categorical variable cat.
 #'
@@ -18,7 +18,7 @@ Summary_function <- function(data, cat, y){
   data %>%
     na.omit() %>%
     group_by({{ cat }}) %>%
-    summarise(minimum = min({{ y }}, na.rm = TRUE),
+    dplyr::summarise(minimum = min({{ y }}, na.rm = TRUE),
               maximum = max({{ y }}, na.rm = TRUE),
               average = mean({{ y }}, na.rm = TRUE),
               range = (max({{ y }}) - min({{ y }})),
