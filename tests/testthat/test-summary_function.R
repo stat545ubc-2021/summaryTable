b41fb9fd790b520ad1223b000f49a7fa08c38392
Charlotte Barclay <cbarcl01@student.ubc.ca>
2021-11-20 16:25:24 -0800

    Add tests using testthat

---
+++ tests/testthat/test-summary_function.R
@@ -0,0 +1,14 @@
+test_that("The sum of the maximums is greater than the minimums", {
+  expect_gte(sum(Summary_function(gapminder, country, pop)$maximum) - sum(Summary_function(gapminder, country, pop)$minimum), 0)
+
+})
+
+test_that("That the grouped variable is a factor", {
+  expect_s3_class(Summary_function(gapminder, country, pop)$country, "factor")
+
+})
+
+test_that("All countries are included in the group_by", {
+  expect_equal(nrow(Summary_function(gapminder, country, pop)), 142)
+
+})
